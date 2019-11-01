fb52ccebd49b30dbae91208bb2f27fcf7c03d6ab
Dmitry Gomzyakov <nicerok11@gmail.com>
2019-11-01 17:45:26 +0500

    havocify part #2

--- havoc_libra.mk
+++ havoc_libra.mk
@@ -7,7 +7,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l.mk)
 
 # Inherit some common havoc stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/libra/libra.mk)
